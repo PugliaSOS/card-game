427b4b27ae5336981816dd0a40ec11b7dbf9ce5e
Dom <dom.corvasce@yandex.com>
2017-01-26 18:37:01 +0100

    Add test case for Card

---
+++ test/card.spec.js
@@ -0,0 +1,22 @@
+const expect = require('./helper').expect;
+
+const Card = require('../src/core/card');
+
+describe('Card', () => {
+  beforeEach(() => {
+    this.card = new Card(2, 'spades');
+  });
+
+  describe('#constructor', () => {
+    it('initializes a new card', () => {
+      expect(this.card.value).to.equal(2);
+      expect(this.card.seed).to.equal('spades');
+    });
+  });
+
+  describe('#toString', () => {
+    it('returns a string representation of card', () => {
+      expect(this.card.toString()).to.equal('2spades');
+    });
+  });
+});
