fe6b72a79181cb32eb65cc0de43f1fb2316d2a96
Mario Lamacchia <mario.lamacchia@nokia.com>
2017-01-27 10:05:29 +0100

    refactor(lint): fix shell.js and scopa/index.js

--- src/demo/utils/shell.js
+++ src/demo/utils/shell.js
@@ -1,14 +1,15 @@
-const read = require('read');
+/* eslint no-console: off */
+// const read = require('read');
 
-const handleStart = (game) => {
+const handleStart = () => {
   console.log();
   console.log('# Game has started');
-}
+};
 
-const handleHand = (game) => {
+const handleHand = () => {
   console.log();
   console.log('## Next hand');
-}
+};
 
 const handleTurn = (game) => {
   const getCard = index => game.currentPlayer.hand.cards[index - 1];
@@ -20,20 +21,20 @@ const handleTurn = (game) => {
   console.log('The following cards are in your hand:');
   console.log(game.currentPlayer.hand.cards.map(c => c.toString()));
   console.log('Which card do you choose? (Give index)');
-  
+
   // read({
   //   prompt: '>'
   // }, (err, choice) => {
   //   game.move(Number(choice - 1));
   // })
   game.move(getCard(1));
-}
+};
 
-const handleEnd = game => {
+const handleEnd = (game) => {
   console.log();
   console.log('Game over');
-  game.players.forEach(p => console.log(`${p.name}'s score: ${p.points}`))
+  game.players.forEach(p => console.log(`${p.name}'s score: ${p.points}`));
   console.log('######');
-}
+};
 
 module.exports = { handleTurn, handleStart, handleHand, handleEnd };
--- src/games/scopa/index.js
+++ src/games/scopa/index.js
@@ -1,12 +1,12 @@
-const getCombinations = arr => {
+const getCombinations = (arr) => {
   if (arr.length < 2) { return [arr]; }
   const sub = getCombinations(arr.slice(1));
   return [
     [arr[0]],
     ...sub.map(c => [arr[0], ...c]),
-    ...sub
+    ...sub,
   ];
-}
+};
 
 const evaluatePossibilities = (game, choice) => {
   let possibilities = game.table.cards
@@ -14,46 +14,41 @@ const evaluatePossibilities = (game, choice) => {
     .map(c => [c]);
   if (!possibilities.length) {
     possibilities = getCombinations(game.table.cards)
-      .filter(c => c.reduce((a,b) => a.value + b.value, 0) === choice.value)
+      .filter(c => c.reduce((a, b) => a.value + b.value, 0) === choice.value);
   }
   return possibilities;
-}
+};
 
-const startHand = game => {
+const startHand = (game) => {
   if (game.currentPlayer.hand.isEmpty()) {
-    game.players.forEach(p => {
-      [...new Array(3)].forEach(_ => p.hand.add(game.deck.draw()));
+    game.players.forEach((p) => {
+      [...new Array(3)].forEach(() => p.hand.add(game.deck.draw()));
     });
   }
-}
+};
 
-const isOver = game => {
-  return game.currentPlayer.hand.isEmpty() && game.deck.isEmpty();
-}
+const isOver = game => game.currentPlayer.hand.isEmpty() && game.deck.isEmpty();
 
-const startGame = game => {
+const startGame = (game) => {
   game.deck.mix();
   startHand(game);
-  [...new Array(4)].forEach(_ => game.table.add(game.deck.draw()));
-}
+  [...new Array(4)].forEach(() => game.table.add(game.deck.draw()));
+};
 
 const playCard = (game, choice) => {
   const card = game.currentPlayer.hand.pick(choice.card);
   const possibilities = evaluatePossibilities(game, card);
   if (possibilities.length) {
-    possibilities[0].forEach(c => {
-      game.currentPlayer.score.add(game.table.pick(c));
-    });
+    possibilities[0].forEach(c => game.currentPlayer.score.add(game.table.pick(c)));
     game.currentPlayer.score.add(choice.card);
 
     if (game.table.isEmpty() && !game.deck.isEmpty()) {
       game.currentPlayer.scopa++;
     }
-
   } else {
     game.table.add(card);
   }
-}
+};
 
 const getScore = (card, type) => {
   switch (type) {
@@ -65,42 +60,42 @@ const getScore = (card, type) => {
       return (card.seed === 'D' && card.value === 7) ? 1 : 0;
   }
   return 1;
-}
+};
 
-const endMetch = game => {
-  ['lunga', 'denari', 'settebello'].forEach(t => {
-    const scores = game.players.map(p => {
-      return p.score.cards.reduce((acc, c) => acc + getScore(c, t), 0);
-    });
+const endMetch = (game) => {
+  ['lunga', 'denari', 'settebello'].forEach((t) => {
+    const scores = game.players.map(p => (
+      p.score.cards.reduce((acc, c) => acc + getScore(c, t), 0)
+    ));
 
-    const maxScore = Math.max.apply(Math, scores);
+    const maxScore = Math.max(...scores);
     if (scores.filter(v => v === maxScore).length === 1) {
       game.players[scores.indexOf(maxScore)].points++;
     }
   });
 
   // Primiera
-  const primieraScores = game.players.map(p => {
-    return 'DCSB'.split('')
-      .map(seed => {
-        return Math.max.apply(
+  const primieraScores = game.players.map(p => (
+    'DCSB'.split('')
+      .map(seed => (
+        Math.max.apply(
           null,
           p.score.cards
             .filter(c => c.seed === seed)
-            .map(c => getScore(c, 'primiera'))
-        );
-      })
-      .reduce((a, b) => a + b, 0);
-  });
-  const primieraMaxScore = Math.max.apply(Math, primieraScores);
+            .map(c => getScore(c, 'primiera')),
+        )
+      ))
+      .reduce((a, b) => a + b, 0)
+  ));
+  const primieraMaxScore = Math.max(...primieraScores);
   if (primieraScores.filter(v => v === primieraMaxScore).length === 1) {
     game.players[primieraScores.indexOf(primieraMaxScore)].points++;
   }
 
   // Scopa
-  game.players.forEach(p => p.points += p.scopa)
-}
+  game.players.forEach(p => p.points += p.scopa);
+};
 
-const startTurn = game => {}
+const startTurn = () => {};
 
 module.exports = { startTurn, startGame, startHand, playCard, endMetch, isOver };
