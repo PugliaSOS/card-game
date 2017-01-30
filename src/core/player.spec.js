bc3e3757ac9bd9897dbc4cb2616c53793b443537
Dom <dom.corvasce@yandex.com>
2017-01-30 11:56:46 +0100

    Refactor(tests): Move test's variables to it blocks in player.spec.js too

--- src/core/player.spec.js
+++ src/core/player.spec.js
@@ -3,10 +3,8 @@ const expect = require('chai').expect;
 const Player = require('./player');
 
 describe('Player', () => {
-  let player;
-
   it('initializes a new player', () => {
-    player = new Player('Dom');
+    const player = new Player('Dom');
 
     expect(player.name).to.equal('Dom');
     expect(player.points).to.equal(0);
