a9c7e2e9b6cdee10d8c8a6875381316fd572858c
Dom <dom.corvasce@yandex.com>
2017-01-30 11:08:38 +0100

    Refactor(tests): Move tests in the same folder of modules

---
+++ src/core/card.spec.js
@@ -0,0 +1,17 @@
+const expect = require('chai').expect;
+
+const Card = require('./card');
+
+describe('Card', () => {
+  const cardData = [2, 'spades'];
+  const card = new Card(...cardData);
+
+  it('initializes a new card', () => {
+    expect(card.value).to.equal(cardData[0]);
+    expect(card.seed).to.equal(cardData[1]);
+  });
+
+  it('returns a string representation of card', () => {
+    expect(card.toString()).to.equal(cardData.join(''));
+  });
+});
